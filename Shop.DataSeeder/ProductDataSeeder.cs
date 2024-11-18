8f28fd88718bb38879708210791109fc8b00463e
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-18 11:46:09 +0100

    q

---
+++ Shop.DataSeeder/ProductDataSeeder.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+using Shop.Shared;
+
+namespace Shop.DataSeeder
+{
+    public class ProductDataSeeder
+    {
+        public static List<Product> GenerateProductsData()
+        {
+            var productsFaker = new Faker<Product>()
+                .RuleFor(p => p.Title, f => f.Commerce.ProductName())
+                .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
+                .RuleFor(p => p.Price, f => f.Random.Double(1, 1000))
+                .RuleFor(p => p.Barcode, f => f.Commerce.Ean13())
+                .RuleFor(p => p.ReleaseDate, f => f.Date.Past());
+
+            return productsFaker.Generate(100);
+        }
+    }
+}
