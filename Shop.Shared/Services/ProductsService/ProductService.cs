6e79e73778d6fc8d49890d7602440c4bbd522e6e
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-18 12:55:10 +0100

    q

---
+++ Shop.Shared/Services/ProductsService/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Shared.Services.ProductsService
+{
+    internal class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AppSettings _appSettings;
+        public Task<ServiceReponse<List<Product>>> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
