c50e223cb741b3c93fe2c5a42fbc46e785c09a14
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-18 11:22:39 +0100

    q

--- Shop.API/Models/DataContext.cs
+++ Shop.API/Models/DataContext.cs
@@ -1,6 +1,19 @@
-﻿namespace Shop.API.Models
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Shared;
+
+namespace Shop.API.Models
 {
-    public class DataContext  
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
     }
 }
