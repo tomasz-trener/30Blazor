50d846835800ab0ddf44e4d2aae4343ce4e6c7ff
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-18 13:14:58 +0100

    q

--- BlazorWebassembly.Client/Program.cs
+++ BlazorWebassembly.Client/Program.cs
@@ -11,7 +11,11 @@ namespace BlazorWebassembly.Client
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //  builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            //Microsoft.Extensions.Http
+           // builder.Services.AddHttpClient
+
 
             await builder.Build().RunAsync();
         }
