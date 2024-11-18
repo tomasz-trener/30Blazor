ccb562414b1f8feccc25ee576cd4d2b15ae1e3da
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-18 15:10:19 +0100

    q

--- Shop.Shared/Services/ProductsService/IProductService.cs
+++ Shop.Shared/Services/ProductsService/IProductService.cs
@@ -9,5 +9,16 @@ namespace Shop.Shared.Services.ProductsService
     public interface IProductService
     {
         Task<ServiceResponse<List<Product>>> GetProductsAsync();
+
+        Task<ServiceResponse<Product>> CreateProductAsync(Product newProduct);
+
+        Task<ServiceResponse<bool>> DeleteProductAsync(int id);
+
+        Task<ServiceResponse<Product>> UpdateProductAsync(Product updatedProduct);
+
+        Task<ServiceResponse<Product>> GetProductAsync(int id);
+
+
+
     }
 }
