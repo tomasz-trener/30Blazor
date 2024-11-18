c7b66e2f8fe11899ece4971d235635e22c705212
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-18 13:01:58 +0100

    q

---
+++ Shop.Shared/Configuration/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Shared.Configuration
+{
+    internal class AppSettings
+    {
+        public AppSettings()
+        {
+            ProductEndpoint = new ProductEndpoint();
+        }
+        public string BaseApiUrl { get; set; }
+
+        public ProductEndpoint ProductEndpoint { get; set; }
+    }
+}
