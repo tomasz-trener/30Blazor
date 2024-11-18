9345cb89982675adc1347a573db6fa4db1a8efe0
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-18 11:05:30 +0100

    Add project files.

---
+++ Shop.Shared/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Barcode { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
