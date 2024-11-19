93a95950d6e3bc40b4e0eb7e4a0165b9bb06917e
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-19 12:03:22 +0100

    q

---
+++ Shop.API/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.API.Services;
+
+namespace Shop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserRegisterDto request)
+        {
+            var response = await _authService.RegisterAsync(
+                new User { Email = request.Email, Name = request.Name }, request.Password
+            );
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginDto request)
+        {
+            var response = await _authService.LoginAsync(request.Email, request.Password);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+    }
+}
