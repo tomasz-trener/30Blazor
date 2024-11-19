cb30c08148e6c668dce9e3954dc7ebe4417e3f97
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-19 09:41:27 +0100

    q

--- Shop.Shared/FilterParams.cs
+++ Shop.Shared/FilterParams.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 namespace Shop.Shared
 {
     public class FilterParams
-    { 
+    {
+        public string Id { get; set; }
         public string Title { get; set; }
         public string Price { get; set; }
 
         public string Barcode { get; set; }
 
+        public string Description { get; set; }
         public string ReleaseDate { get; set; }
     }
 }
