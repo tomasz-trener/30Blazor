4849f959875bf473f49f9e2412203116b90ac080
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-19 16:05:54 +0100

    q

--- BlazorWebassembly.Client/Program.cs
+++ BlazorWebassembly.Client/Program.cs
@@ -1,4 +1,5 @@
 using Blazored.LocalStorage;
+using BlazorWebassembly.Client.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
