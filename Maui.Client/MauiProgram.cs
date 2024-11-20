bd1d82a0c6db5093d7aaf3b8a1660388b3c46120
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-20 11:53:11 +0100

    q

--- Maui.Client/MauiProgram.cs
+++ Maui.Client/MauiProgram.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿
+using Maui.Client.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Shop.Shared.Configuration;
+using Shop.Shared.Services.ProductsService;
+using System;
 
 namespace Maui.Client
 {
@@ -27,10 +32,41 @@ namespace Maui.Client
         private static void ConfigureServices(IServiceCollection services)
         {
             var appSettings = ConfigureAppSettings(services);
+            ConfigureAppServices(services, appSettings);
+            ConfigureHttpClients(services, appSettings);
+            ConfigureViewModels(services);
+            ConfigureViews(services);
 
         }
 
-        private static void ConfigureAppSettings(IServiceCollection services)
+        private static void ConfigureAppServices(IServiceCollection services, AppSettings appSettings)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+
+        private static void ConfigureViews(IServiceCollection services)
+        {
+            services.AddSingleton<MainPage>();
+        }
+
+        private static void ConfigureViewModels(IServiceCollection services)
+        {
+            services.AddSingleton<ProductViewModel>();
+        }
+
+        private static void ConfigureHttpClients(IServiceCollection services, AppSettings appSettings)
+        {
+            var uriBuilder = new UriBuilder(appSettings.BaseApiUrl)
+            {
+                Path = appSettings.ProductEndpoint.BaseUrl
+            };
+
+            services.AddHttpClient<IProductService, ProductService>(client => client.BaseAddress = uriBuilder.Uri);
+        }
+
+  
+
+        private static AppSettings ConfigureAppSettings(IServiceCollection services)
         {
             var appSettingsSection = new AppSettings()
             {
@@ -43,6 +79,14 @@ namespace Maui.Client
                     UpdateProduct = "",
                 }
             };
+            services.Configure<AppSettings>(options =>
+            {
+                options.BaseApiUrl = appSettingsSection.BaseApiUrl;
+                options.ProductEndpoint = appSettingsSection.ProductEndpoint;
+            });
+
+            return appSettingsSection;
+
 
         }
     }
