bd6bcbc244beb995b52f317cae29973ea9190072
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-20 11:33:20 +0100

    q

--- Maui.Client/MauiProgram.cs
+++ Maui.Client/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Shop.Shared.Configuration;
 
 namespace Maui.Client
 {
@@ -19,7 +20,30 @@ namespace Maui.Client
     		builder.Logging.AddDebug();
 #endif
 
+            ConfigureServices(builder.Services);
             return builder.Build();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            var appSettings = ConfigureAppSettings(services);
+
+        }
+
+        private static void ConfigureAppSettings(IServiceCollection services)
+        {
+            var appSettingsSection = new AppSettings()
+            {
+                BaseApiUrl = "https://supershopblazor-h6b6agdgbeewdzgm.northeurope-01.azurewebsites.net",
+                ProductEndpoint = new ProductEndpoint()
+                {
+                    BaseUrl = "api/product/",
+                    GetProducts = "",
+                    CreateProduct = "",
+                    UpdateProduct = "",
+                }
+            };
+
         }
     }
 }
